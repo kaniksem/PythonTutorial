3896e98fd987addebdcf4439ae4fb4245ed73962
kanik <kanik.sem@gmail.com>
2017-04-14 23:33:08 -0700

    please commit

--- Section_5.py
+++ Section_5.py
@@ -1,1 +1,14 @@
+print('this is the beginning of lecture 21')
 print("hello world")
+print(1+2)
+print()
+print('We can even include "quotes" in strings')
+print("You can even " + 'concatenate strings in Python')
+print('this is the end of lecture 21 ')
+# this is a python comment
+
+
+
+age=input("enter your age")
+print("your age is {}".format(age))
+
