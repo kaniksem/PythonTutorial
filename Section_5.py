59e3d24e8f3c804169f60a1d43409b8c7bb94b65
kanik <kanik.sem@gmail.com>
2017-04-16 16:36:56 -0700

    commit

--- Section_5.py
+++ Section_5.py
@@ -1,14 +1,53 @@
-print('this is the beginning of lecture 21')
-print("hello world")
-print(1+2)
-print()
-print('We can even include "quotes" in strings')
-print("You can even " + 'concatenate strings in Python')
-print('this is the end of lecture 21 ')
+# print('this is the beginning of lecture 21')
+# print("hello world")
+# print(1+2)
+# print('We can even include "quotes" in strings')
+# print("You can even " + 'concatenate strings in Python')
+# print('this is the end of lecture 21 ')
 # this is a python comment
 
+# print()
+# print('this is the beginning of lecture 22')
+# name = input('Please enter your name')
+# print('Hello' + ' ' + name)
+# print("This string has \nbeen split over \n several times")
+# tabbedString='1\t2\t3\t4\t'
+# # print(tabbedString)
+# print('The pet owner said that "he\'s out of his mind"')
+# print("The pet owner said that \"he's out of his mind\"")
+# splitString="""Triple
+# quotes
+# are
+# used
+# in Python
+# for
+# spliting
+# strings
+# without using back-slash n"""
+# print(splitString)
+# print('this is the end of lecture 22')
 
+print()
+print('this is the beginning of lecture 23')
+# age=30
+# age2=20
+# #print('Your age is' + " " + age) - this would generate an error
+# print('Your age is ' + str(age))
+# a=14
+# b=10
+# print(a+b)
+# print(a / b)  # Divides a by b and returns a float
+# print(a//b)  # Divides a by b and returns an integer
+# print(a % b)
+#
+# for i in range(3, 10):
+#     print(i)
 
-age=input("enter your age")
-print("your age is {}".format(age))
+parrot='Norwegian Blue'
+print(parrot[3])  # Prints the 4th letter since the string starts at index 0
+#print(parrot[50])   IndexError: string index out of range
+print(parrot[-4])
+print(parrot[1:3])  # this will print 3-1==2 characters starting at index 1
+print(parrot[10:13])
+print('this is the end of lecture 23')
 
