1d9e93e8d34f245372019c68d044ccff1be5af8d
kanik <kanik.sem@gmail.com>
2017-04-22 15:00:40 -0700

    commit

---
+++ Section_7.py
@@ -0,0 +1,20 @@
+# Python Lists
+country_list = ['America', 'Canada', 'Mexico', 'Germany', 'France']
+# index = 1
+# j=1
+#
+# for country in country_list:
+#     print('{} country is '.format(index)+country)
+#     index += 1
+#
+# country_list.append('India')
+# for country in country_list:
+#     print('{} country is '.format(j)+country)
+#     j += 1
+
+# concatenating lists in python
+even=[2, 4, 6, 8]
+odd=[1,3,5,7]
+allnumbers=even+odd
+allnumbersSorted=allnumbers.sorted()
+print(allnumbersSorted)
